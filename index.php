1dd1df5e7eb009bb643e08bd20f108927c71a3f3
Mauricio Urrego <mauricio@dotdotdot.email>
2022-07-24 14:21:57 +0200

    Initial commit.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<a href = "mailto:hello@dotdotdot.email?subject=hello&body=Hello, %0A %0A %0A My name is... %0A %0A I met you at... %0A %0A You could probably help me with... %0A %0A You can contact me like this... or like this... %0A %0A %0A Thank you Ellipsis">
+    <p class="title">
+        say <span id="hello">hello...</span>
+    </p>
+</a>
+</body>
+</html>
